0583cbbf0d19807db8419d52358549f821e0f792
Victor Mingueza <victor.mingueza@countdown.co.nz>
2022-08-09 08:52:20 +1200

    Basic http

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	i "vm.io/payroll/invoice"
@@ -16,6 +19,20 @@ func main() {
 	s := GetInvoiceService()
 	invoices := s.GetAllByFinancialYear(uint16(time.Now().Year()))
 	fmt.Printf("Obtained %v invoices", len(invoices))
+
+	http.HandleFunc("/invoices", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		err := json.NewEncoder(w).Encode(invoices)
+
+		if err != nil {
+			log.Fatalf("Couldn't serialize invoices %v", err.Error())	
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Couldn't start the server: %v", err.Error())
+	}
 }
 
 func GetInvoiceService() i.InvoiceService {
